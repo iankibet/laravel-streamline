3fd4baf6568f5ab2b660f5d22a6d334f97fe343c
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2024-08-22 07:22:06 +0300

    WIP streamline

--- src/StreamlineComponent.php
+++ src/StreamlineComponent.php
@@ -13,4 +13,29 @@ abstract  class StreamlineComponent
         }
         return request()->validate($rules);
     }
+
+    public function onMounted()
+    {
+        //return class instance
+        return $this->toArray();
+    }
+
+    protected function toArray()
+    {
+        // get public properties of the class instance then return them as an array
+        $data = [];
+        $classVars = call_user_func('get_object_vars', $this);
+        // remove the rules property
+        unset($classVars['rules']);
+        // get all the public functions of the class instance
+        $methods = get_class_methods($this);
+        $data['methods'] = [];
+        $data['properties'] = $classVars;
+        foreach ($methods as $method) {
+            if ($method !== 'toArray' && $method !== 'onMounted') {
+                $data['methods'][] = $method;
+            }
+        }
+        return $data;
+    }
 }
